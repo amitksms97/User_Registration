9379015db5c33b267aa8cd66764fb4c7845a7a7f
amtksms97 <amtksms97@gmail.com>
2020-11-05 17:33:52 +0530

    Added code to find validate user first name

--- User_Registration.sh
+++ User_Registration.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo "Welcome to User Registration System"
 
-
+echo "enter the name:"
+read name
+if [[ "$name" =~ ^[A-Z][a-z]$ ]]
+then
+    echo "valid name"
+else
+    echo "Invalid name"
+fi
